d0570fd8bca757870fd94377fe9847e9c7d4a439
Vivek Kumar <k99vivek@gmil.com>
2018-08-18 11:42:56 -0700

    added edge detection

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/shape.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+#include <string>
 
 const int MAX_NEIGHBORS = 6;
 
@@ -45,40 +47,46 @@ cv::Mat getPolyMask(cv::Point* vertices, int npts, int rows, int cols)
     return mask;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    cv::Mat img = cv::imread("../mrbluesky.jpg", CV_LOAD_IMAGE_COLOR);
+    std::string filename = "../";
+    filename+= argv[1];
 
-    const int NUM_NODES = 20;
-    Node* pixelCorners[NUM_NODES][NUM_NODES];
+    cv::RNG rng;
 
-    for (int c = 0; c < NUM_NODES; c++)
-        for (int r = 0; r < NUM_NODES; r++)
-             pixelCorners[c][r] = new Node(
-                                           cv::Point(c*img.cols/(NUM_NODES-1),
-                                                     r*img.rows/(NUM_NODES-1)),
-                                           img);
+    cv::Mat img = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+    cv::Mat edgy, dilated, eroded, blurry;
+    cv::Mat blank = cv::Mat::zeros(img.rows, img.cols, CV_8UC3);
 
-    for (int c = 0; c < NUM_NODES - 1; c++)
-        for (int r = 0; r < NUM_NODES - 1; r++)
-        {
-            cv::Point neighbors[4];
-            neighbors[0] = pixelCorners[c  ][r  ]->pos_;
-            neighbors[1] = pixelCorners[c  ][r+1]->pos_;
-            neighbors[2] = pixelCorners[c+1][r+1]->pos_;
-            neighbors[3] = pixelCorners[c+1][r  ]->pos_;
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
 
-            cv::Mat mask = getPolyMask(neighbors, 4, img.rows, img.cols);
-
-            cv::Scalar avgColor = cv::mean(img, mask);
-            cv::fillConvexPoly(img, neighbors, 4, avgColor);
-        }
-
+    int blurSize = img.cols/100 - !(img.cols/100 % 2);
+    //cv::GaussianBlur(img, blurry, cv::Size(blurSize, blurSize), 0);
+    cv::Canny(img, edgy, 100, 200);
+    //cv::GaussianBlur(edgy, blurry, cv::Size(3, 3), 0);
+    //blurry = (blurry != 0);
+    // cv::dilate(edgy, dilated, cv::Mat::ones(4,4,0));
+    // cv::erode(dilated, eroded, cv::Mat::ones(2,2,0));
+    cv::findContours(edgy, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    for (int c = 0; c < contours.size(); c++)
+        if (cv::contourArea(contours[c]) > 10)
+            cv::drawContours(blank,
+                            contours,
+                            c,
+                            cv::Scalar(rng.uniform(100, 255),
+                                        rng.uniform(100, 255),
+                                        rng.uniform(100, 255),1));
 
     cv::namedWindow("Mr. Blue Sky", CV_WINDOW_NORMAL);
-    cv::resizeWindow("Mr. Blue Sky", 1000, 1000);
+    cv::resizeWindow("Mr. Blue Sky", 500, 500);
     cv::imshow("Mr. Blue Sky", img);
+
+    cv::namedWindow("output", CV_WINDOW_NORMAL);
+    cv::resizeWindow("output", 500, 500);
+    cv::imshow("output", blank);
 
     cv::waitKey();
     cv::destroyAllWindows();
 }
+
